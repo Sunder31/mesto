f55f46866c5ea36d4b36534a9b28ec263776eaca
Andrey Balyakin <sunder33@yandex.ru>
2023-03-07 16:06:39 +0300

    исправлены ошибки после первой проверки работы

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,26 @@ const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const formElement = popupElement.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-about');
+let nameInput = formElement.querySelector('.form__item_el_name');
+let jobInput = formElement.querySelector('.form__item_el_about');
 let nameValue = document.querySelector('.profile__name');
 let jobValue = document.querySelector('.profile__about');
 
 
 
 const openPopup = function() {
-    popupElement.classList.add('popup_is-opened');
+    popupElement.classList.add('popup_opened');
     nameInput.value = nameValue.textContent ;
     jobInput.value = jobValue.textContent;
 };
 
 const closePopup = function() {
-    popupElement.classList.remove('popup_is-opened');
+    popupElement.classList.remove('popup_opened');
 };
 
 const closePopupByClickOnOverlay = function(event){
     if (event.target !== event.currentTarget) {
-    return;
+        return;
     }
     closePopup();
 }
